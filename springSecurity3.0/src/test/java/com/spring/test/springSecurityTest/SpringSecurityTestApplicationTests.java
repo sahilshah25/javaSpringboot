48454f46a6c0d14b2ce79063ea4180831e9aba68
sahilshah25 <sahilshah84621@gmail.com>
2024-12-15 17:41:54 +0530

    Add files via upload

---
+++ springSecurity3.0/src/test/java/com/spring/test/springSecurityTest/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.test.springSecurityTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
